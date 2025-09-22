a6c8a0cb59327abd2d0fdb04cc829934c99ac1eb
stanleylew5 <stanley43595@gmail.com>
2025-09-22 00:21:59 -0700

    working bars

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -6,7 +6,7 @@ import Lothian from "./lothian";
 
 const Header = () => {
   return (
-    <div className="mx-auto w-5/6">
+    <div className="border-reats-blue-100 border-b-1 px-[10%]">
       <div className="mt-8 ml-4 flex items-center">
         <Image src={logo} alt="logo" className="w-[75px]" />
         <p className="text-reats-blue-100 ml-2 text-xl font-semibold">
@@ -15,8 +15,23 @@ const Header = () => {
       </div>
       <Tabs defaultValue="glasgow" className="mt-8">
         <TabsList>
-          <TabsTrigger value="glasgow">Glasgow</TabsTrigger>
-          <TabsTrigger value="lothian">Lothian</TabsTrigger>
+          <TabsTrigger
+            value="glasgow"
+            className={
+              "after:bg-reats-blue-200 data-[state=active]:after:animate-border-grow relative after:absolute after:-bottom-1 after:left-0 after:h-[3px] after:w-0 after:transition-all after:duration-300 after:content-[''] data-[state=active]:after:w-full"
+            }
+          >
+            Glasgow
+          </TabsTrigger>
+
+          <TabsTrigger
+            value="lothian"
+            className={
+              "after:bg-reats-blue-200 data-[state=active]:after:animate-border-grow relative after:absolute after:-bottom-1 after:left-0 after:h-[3px] after:w-0 after:transition-all after:duration-300 after:content-[''] data-[state=active]:after:w-full"
+            }
+          >
+            Lothian
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="glasgow">
           <Glasgow />
