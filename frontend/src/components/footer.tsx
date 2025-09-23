0258034f468f863a6b90c461aa897da3fbe51aed
stanleylew5 <stanley43595@gmail.com>
2025-09-23 11:24:44 -0700

    update footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,5 +1,12 @@
+import logo from "@/public/newlogo.webp";
+import Image from "next/image";
 const Footer = () => {
-  return <div className="pb-4 text-center">&copy; 2025 R&apos;Eats</div>;
+  return (
+    <div className="flex items-center justify-center pb-4 text-center">
+      <Image src={logo} alt="logo" className="mx-2 h-8 w-8" />
+      <p>2025 R&apos;Eats &copy;</p>
+    </div>
+  );
 };
 
 export default Footer;
